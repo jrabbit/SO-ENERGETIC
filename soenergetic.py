22f218ac86896782ca6e5341c6c9c027fad4a3a0
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-25 17:45:18 -0500

    imports

--- soenergetic.py
+++ soenergetic.py
@@ -1,5 +1,13 @@
 import pygame
+import random
+from pygame.locals import *
+
+if not pygame.font: 
+    print 'Warning, fonts disabled'
+if not pygame.mixer: 
+    print 'Warning, sound disabled'
 
 #DO WORK
 
 stages = ['spark', 'zeal', 'moxie', 'vigor', 'drive', 'fire']
+
